49da1cd4ced46577dace9a772e1bae997bad90f4
Michał Balicki <michalbalicki@o2.pl>
2019-11-21 02:08:26 +0100

    Added content of piece class

--- Carthagina Rome Battle/Piece.cpp
+++ Carthagina Rome Battle/Piece.cpp
@@ -2,11 +2,22 @@
 
 
 
-Piece::Piece()
+Piece::Piece(int id, COORD position, bool white, std::shared_ptr<MeridorGraphics::Sprite> sprite)
 {
+	this->id = id;
+	this->position = position;
+	this->white = white;
+	this->sprite = sprite;
 }
 
-
-Piece::~Piece()
+void Piece::Draw()
 {
+	sprite->SetX(240 + 90 * position.X);
+	sprite->SetY(40 + 90 * position.Y);
+	sprite->Draw();
+}
+
+void Piece::Update(int x, int y)
+{
+	position = { static_cast<short>(x),static_cast<short>(y) };
 }
--- Carthagina Rome Battle/Piece.h
+++ Carthagina Rome Battle/Piece.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "Sprite.h"
+
+
 class Piece
 {
+	std::shared_ptr<MeridorGraphics::Sprite> sprite;
+	int id;
+	COORD position;
+	bool white;
+	bool moved = false;
+
 public:
-	Piece();
-	~Piece();
+	Piece(int id, COORD position, bool white, std::shared_ptr< MeridorGraphics::Sprite> sprite);
+	void Draw();
+	void Update(int x, int y);
 };
 
